4c69e09d0276940d0877230b290d88848ce6cfab
Etienne Delay <etienne.delay@cirad.fr>
2024-07-09 12:45:45 +0000

    etuder le nombre de réplication du modele

--- model/R/replciation_boxplot.R
+++ model/R/replciation_boxplot.R
@@ -2,7 +2,7 @@ library("dplyr")
 library("ggplot2")
 
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
-
+rm(list = ls())
 df <- read.csv("../data_simu/replication.csv", header = T, sep = ",")
 
 
@@ -10,42 +10,36 @@ df <- read.csv("../data_simu/replication.csv", header = T, sep = ",")
 # créer des groupes dans un data frame
 # et on plotera les boxplot correspondant par groupe
 
-sample.v <- seq(from = 2, to = 10, by = 2) # vecteur du nombre de tirage
+sample.v <- seq(from = 2, to = 40, by = 2) # vecteur du nombre de tirage
 
 df.gp <- data.frame()
 for(i in sample.v){
-  for(j in 1:10){
+  for(j in 1:3){
     a <- df %>% slice_sample(n = i, replace = T)
     a$gp <- j
     df.gp <- rbind(df.gp, a)
   }
   ggplot(data = df.gp)+
     geom_boxplot(aes(x = as.factor(gp), y = ef))+
-    labs(x  = "sample", title = paste("nombre de réplication:", i))+
-    geom_hline(yintercept = 0.3945418, linetype='dotted', col = 'grey')+
+    labs(x  = "group", title = paste("number of simulations sampled:", i))+
+    geom_hline(yintercept = 0.3925234, linetype='dotted', col = 'grey')+
     ylim(c(0.30,0.50))+
     theme_bw()
   ggsave(paste0("../img/sample/ef_sample_",i,".png"))
   
-  ggplot(data = df.gp)+
-    geom_boxplot(aes(x = as.factor(gp), y = pop))+
-    labs(x  = "sample", title = paste("nombre de réplication:", i))+
-    # ylim(c(0.30,0.50))+
-    theme_bw()
-  ggsave(paste0("../img/sample/pop_sample_",i,".png"))
   
   ggplot(data = df.gp)+
     geom_boxplot(aes(x = as.factor(gp), y = yield))+
-    labs(x  = "sample", title = paste("nombre de réplication:", i))+
-    geom_hline(yintercept = 691.8634, linetype='dotted', col = 'grey')+
-    ylim(c(600,750))+
+    labs(x  = "group", title = paste("number of simulations sampled:", i))+
+    geom_hline(yintercept = 689.3767, linetype='dotted', col = 'grey')+
+    #ylim(c(689.3767))+
     theme_bw()
   ggsave(paste0("../img/sample/yield_sample_",i,".png"))
   
   ggplot(data = df.gp)+
     geom_boxplot(aes(x = as.factor(gp), y = herd))+
-    labs(x  = "sample", title = paste("nombre de réplication:", i))+
-    geom_hline(yintercept = 88, linetype='dotted', col = 'grey')+
+    labs(x  = "group", title = paste("number of simulations sampled:", i))+
+    geom_hline(yintercept = 89, linetype='dotted', col = 'grey')+
     ylim(c(50,100))+
     theme_bw()
   ggsave(paste0("../img/sample/herd_sample_",i,".png"))
