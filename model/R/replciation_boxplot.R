ef21c21f8d0720c60b82fbc63593824d1b57c2ed
Etienne Delay <etienne.delay@cirad.fr>
2024-07-11 12:16:28 +0000

    sur zise avec facet wrap label boh

--- model/R/replciation_boxplot.R
+++ model/R/replciation_boxplot.R
@@ -37,9 +37,9 @@ ggplot(df_group, aes(group=size, x=size))+
 
 
 ggplot(df_group, aes( x=group))+
-  geom_boxplot(aes(y=yield , group=group),orientation = "x")+
+  geom_boxplot(aes(y=ef , group=group),orientation = "x")+
   theme_light() +
-  facet_wrap(~size)
+  facet_wrap(~size, labeller = "label_both")
 
 
 df_group$pop %>% unique()
