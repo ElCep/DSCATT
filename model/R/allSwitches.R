64e3a31057c1f764234fb62d4ac7e8f3fea77341
chapinux <paul.chapron@gmail.com>
2025-05-28 16:08:31 +0200

    chnage la palette de couleurs des courbes de bifurcations pour le papier JASSS

--- model/R/allSwitches.R
+++ model/R/allSwitches.R
@@ -3,6 +3,7 @@ library(reshape2)
 library(scales)
 library(dplyr)
 library(stringr)
+library(RColorBrewer)
 
 buildImage = function(dynamicName, dirPath) {
   fifi = paste0(dirPath, dynamicName,".csv")
@@ -29,17 +30,29 @@ buildImage = function(dynamicName, dirPath) {
   #   ylab(dynamicName)+
   #   geom_vline(xintercept=25, linetype='dotted', col = 'black')+
   #   labs(color="Scenario")
-  custom_pal<-  c("#7a7a7a",hue_pal()(9))
-  # base onmly dataframe
+  custom_pal<-  c("black", #base
+                  "saddlebrown", #mulching
+                  "springgreen4", #tress
+                  "green1", #grazing
+                  "hotpink", #LSU1
+                  "darkmagenta", #LSU full
+                  "gold1", #No solidarity     
+                  "turquoise2", #Rainfall 
+                  "red2",# rotation
+                  "navy" #own fallow
+                  )
+  #custom_pal <-  c("black", brewer.pal(9, "Set1"))
+ 
+   # base onmly dataframe
   baseline <- all %>%  filter(variable=="Base")
   all<- filter(all,years>23)  
   ploplot = ggplot(all, aes(x=years))+
     geom_line( aes(y=value, color=variable,), linewidth = 0.6)+
-    geom_line(data = baseline, aes(y=value, color=variable,),linetype="twodash", linewidth = 0.6)+
+    geom_line(data = baseline, aes(y=value, color=variable,), linewidth = 0.4)+
     theme_light()+
     xlab("Years")+
-    ylab("pop")+
-    geom_vline(xintercept=25, linetype='dotted', col = 'black')+
+    ylab(dynamicName)+
+    geom_vline(xintercept=25, linetype='dotted', col = 'grey15')+
     scale_colour_manual(values=custom_pal)+
     labs(color="Scenario")
   
@@ -47,13 +60,14 @@ buildImage = function(dynamicName, dirPath) {
   ggsave(pngFileName, ploplot, width=1500, height = 891, units = "px", dpi=150)
 }
 
+
 dynamicNames = c("yqs","rqs","nitrogen", "effective fallow", "SQ x Nitrogen", "population",
                  "herd size", "millet yield", "loan", "foodStress" )
-lapply(dynamicNames, buildImage, "~/dev/DSCATT/img_article_JASSS/data_courbes_bifurcations/")
+lapply(dynamicNames, buildImage, "~/tmp/data_courbes_bifurcations/")
 
 
 
-fifi = "~/dev/DSCATT/img_article_JASSS/data_courbes_bifurcations/population.csv"
+fifi = "~/DSCATT/img_article_JASSS/data_courbes_bifurcations/population.csv"
 df = read.csv(fifi, header = T, sep=",", fileEncoding = "UTF-8", check.names = F)
 
 traceColumn = function(i){
@@ -68,10 +82,12 @@ all = do.call(rbind, traceList)
 names(all)=c("variable","value", "years")
 all$value = as.numeric(all$value) 
 all$years = as.numeric(all$years)
-all$variable = as.factor()
+all$variable = as.factor(all$variable)
 
 all$variable %>% as.factor() %>% levels
 
+
+all %>%  summary
 
 
 #custom color palette : grey then standard discret ggplot2 palette
